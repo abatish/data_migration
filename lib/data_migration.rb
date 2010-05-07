dc7186aa50c9b6beef9ee5db1bdd63f74fbffa19
Dane Harrigan <dane.harrigan@gmail.com>
2010-05-07 12:36:02 -0400

    - lib/data_migration.rb moved all method_name logic to create_method_name method

--- lib/data_migration.rb
+++ lib/data_migration.rb
@@ -3,22 +3,42 @@ module ActiveRecord
     class << self
       def add_data(obj, *args)
         args = args.pop
-        method_name = ('find_or_create_by_'+create_method_name(args)).to_sym
-        #obj = obj.classify.constantize
+        method_name = create_method_name(
+          :method => :add_data,
+          :force_create => args[:force_create],
+          :arguments => args
+        )
+
+        args.delete :force_create if args[:force_create]
         obj.send(method_name, args)
       end
 
       def remove_data(obj, *args)
         args = args.pop
-        method_name = ('find_by_'+create_method_name(args)).to_sym
-        #obj = obj.classify.constantize
+        method_name = create_method_name(
+          :method => :remove_data,
+          :arguments => args
+        )
+
         result = obj.send(method_name, *args.values)
         result.destroy if result
       end
 
       private
-      def create_method_name(args)
-        args.keys.join('_and_')
+      def create_method_name(*args)
+        args = args.pop
+        case args[:method]
+          when :add_data
+            method_name = args[:force_create] ? 'create' : 'find_or_create_by_'
+          when :remove_data
+            method_name = 'find_by_'
+        end
+
+        unless args[:force_create]
+          method_name = (method_name+args[:arguments].keys.join('_and_')).to_sym
+        end
+
+        return method_name
       end
     end
   end
