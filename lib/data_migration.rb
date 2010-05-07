d0e1df227666237905a86ca07182cf68fe138434
Dane Harrigan <dane.harrigan@gmail.com>
2010-05-06 23:40:35 -0400

    - lib/data_migration.rb properly added add_data and remove_data methods to ActiveRecord::Migration

--- lib/data_migration.rb
+++ lib/data_migration.rb
@@ -1,17 +1,17 @@
 module ActiveRecord
-  module ConnectionAdapters # :nodoc:
-    module SchemaStatements
+  class Migration
+    class << self
       def add_data(obj, *args)
         args = args.pop
         method_name = ('find_or_create_by_'+create_method_name(args)).to_sym
-        obj = obj.classify.constantize
+        #obj = obj.classify.constantize
         obj.send(method_name, args)
       end
 
       def remove_data(obj, *args)
         args = args.pop
         method_name = ('find_by_'+create_method_name(args)).to_sym
-        obj = obj.classify.constantize
+        #obj = obj.classify.constantize
         result = obj.send(method_name, *args.values)
         result.destroy if result
       end
